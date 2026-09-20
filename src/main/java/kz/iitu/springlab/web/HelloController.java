be3df334a24125a644b8078fa05793ccdd4ad73d
Artyom Rot <39655@iitu.edu.kz>
2026-09-20 21:34:45 +0500

    lab1

--- src/main/java/kz/iitu/springlab/web/HelloController.java
+++ src/main/java/kz/iitu/springlab/web/HelloController.java
@@ -1,7 +1,9 @@
 package kz.iitu.springlab.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
@@ -13,7 +15,9 @@ public class HelloController {
     private String owner;
 
     @GetMapping("/hello")
-    public Greeting hello(@RequestParam(defaultValue = "world") String name) {
+    public Greeting hello(
+            @RequestParam(defaultValue = "world") String name) {
+
         return new Greeting(
                 "Hello, " + name + "!",
                 owner,
@@ -30,6 +34,26 @@ public class HelloController {
         );
     }
 
+    @GetMapping("/factorial")
+    public FactorialResult factorial(
+            @RequestParam(defaultValue = "0") int n) {
+
+        if (n < 0 || n > 20) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Number n must be between 0 and 20"
+            );
+        }
+
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return new FactorialResult(n, result);
+    }
+
     public record Greeting(
             String message,
             String owner,
@@ -40,5 +64,10 @@ public class HelloController {
             String owner,
             String javaVersion,
             int cpuCores
+    ) {}
+
+    public record FactorialResult(
+            int n,
+            long factorial
     ) {}
 }
